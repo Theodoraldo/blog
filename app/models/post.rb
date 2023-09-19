cffd95c42c0cb9b7600d186de03a578f0e57b3b9
Theodoraldo <theodoraldo@gmail.com>
2023-09-19 16:49:43 +0000

    Add method which returns the 5 most recent comments for a given post.

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,4 +4,8 @@ class Post < ApplicationRecord
   def update_user_post_count
     author.increment!(:posts_counter)
   end
+
+  def recent_comments_post(limit = 5)
+    comments.order(created_at: :desc).limit(limit)
+  end
 end
