692f5e1ec1acf978f94bd8423ad392d2f5f78a00
Theodoraldo <theodoraldo@gmail.com>
2023-09-27 16:28:58 +0000

    Add method to limit word

--- app/models/post.rb
+++ app/models/post.rb
@@ -16,4 +16,15 @@ class Post < ApplicationRecord
   def recent_comments_post(limit = 5)
     comments.order(created_at: :desc).limit(limit)
   end
+
+  def limit_sentence(content)
+    words = content.split
+    if words.count > 30
+      limited_words = words.take(30)
+      limited_sentence = limited_words.join(' ') + ' . . .'
+      return limited_sentence
+    else 
+      return content
+    end
+  end
 end
