7f326f8705d37237529af891e8302fbc081a50d5
Theodoraldo <theodoraldo@gmail.com>
2023-10-03 03:14:45 +0000

    N+1 problem solved

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
   def index
     @user = User.find(params[:user_id])
-    @posts = @user.posts
+    @posts = @user.posts.preload(:comments)
   end
 
   def show
