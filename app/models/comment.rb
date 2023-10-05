981cbd147bbb1f7e347e6f9031d3e2e0a306b464
Theodoraldo <theodoraldo@gmail.com>
2023-10-05 05:08:42 +0000

    Modify files

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,16 +3,11 @@ class Comment < ApplicationRecord
   belongs_to :post
 
   after_create :update_comments_counter
-
-  after_destroy :decrement_comments_counter
+  after_destroy :update_comments_counter
 
   private
 
   def update_comments_counter
-    post.increment!(:comments_counter)
-  end
-
-  def decrement_comments_counter
-    post.decrement!(:comments_counter)
+    post.update(comments_counter: post.comments.count)
   end
 end
