54130d99b38d695f698b3008d5da2615c073e471
Theodoraldo <theodoraldo@gmail.com>
2023-10-06 10:56:31 +0000

    Remove linter errors

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -3,16 +3,16 @@ class Api::V1::CommentsController < ApplicationController
     post = Post.find(params['post_id'])
     comments = post.comments
     render json: comments
-    rescue ActiveRecord::RecordNotFound
-      render json: { error: 'Post not found' }, status: :not_found
+  rescue ActiveRecord::RecordNotFound
+    render json: { error: 'Post not found' }, status: :not_found
   end
-    
+
   def create
     if current_user.nil?
       render json: { error: 'User not found' }, status: :not_found
       return
     end
-    
+
     @post = Post.find(params[:post_id])
     @comment = @post.comments.new(
       text: params['text'],
