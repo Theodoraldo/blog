d7f12487652928663c4753c6b949f3d713c0dc61
SAZZAD1Q2 <kurulia115@gmail.com>
2023-10-06 14:12:35 +0600

    add api endpoint to list all post by user

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -6,4 +6,22 @@ class Api::V1::CommentsController < ApplicationController
         rescue ActiveRecord::RecordNotFound
           render json: { error: 'Post not found' }, status: :not_found
       end
+      def create
+        if current_user.nil?
+          render json: { error: 'User not found' }, status: :not_found
+          return
+        end
+        @post = Post.find(params[:post_id])
+        @comment = @post.comments.new(
+          text: params['text'],
+          author: current_user
+        )
+    
+        if @comment.save
+          render json: post, status: :created
+        else
+          render json: post.errors, status: :unprocessable_entity
+        end
+      end
+    end
 end
