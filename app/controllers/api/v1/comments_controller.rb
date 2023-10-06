bd5d22f6b8f00f604bd91eb2781e85e47b7cfc4b
SAZZAD1Q2 <kurulia115@gmail.com>
2023-10-06 14:10:11 +0600

    add api endpoint to list all comments by user

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -1,2 +1,9 @@
 class Api::V1::CommentsController < ApplicationController
+    def index
+        post = Post.find(params['post_id'])
+        comments = post.comments
+        render json: comments
+        rescue ActiveRecord::RecordNotFound
+          render json: { error: 'Post not found' }, status: :not_found
+      end
 end
