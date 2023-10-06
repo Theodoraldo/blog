745804ca44a665385b8ac95eecf1c68ce5b720f7
Theodoraldo <theodoraldo@gmail.com>
2023-10-06 10:48:55 +0000

    Modify api controller methods

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -1,27 +1,28 @@
 class Api::V1::CommentsController < ApplicationController
-    def index
-        post = Post.find(params['post_id'])
-        comments = post.comments
-        render json: comments
-        rescue ActiveRecord::RecordNotFound
-          render json: { error: 'Post not found' }, status: :not_found
-      end
-      def create
-        if current_user.nil?
-          render json: { error: 'User not found' }, status: :not_found
-          return
-        end
-        @post = Post.find(params[:post_id])
-        @comment = @post.comments.new(
-          text: params['text'],
-          author: current_user
-        )
+  def index
+    post = Post.find(params['post_id'])
+    comments = post.comments
+    render json: comments
+    rescue ActiveRecord::RecordNotFound
+      render json: { error: 'Post not found' }, status: :not_found
+  end
     
-        if @comment.save
-          render json: post, status: :created
-        else
-          render json: post.errors, status: :unprocessable_entity
-        end
-      end
+  def create
+    if current_user.nil?
+      render json: { error: 'User not found' }, status: :not_found
+      return
     end
+    
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.new(
+      text: params['text'],
+      author: current_user
+    )
+
+    if @comment.save
+      render json: post, status: :created
+    else
+      render json: post.errors, status: :unprocessable_entity
+    end
+  end
 end
